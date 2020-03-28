1f10f657335fac78c568930c7f8f0e59e12f6716
Nathan Bowdish <djnatronic@gmail.com>
2020-03-28 16:21:48 -0700

    most updated

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ var timeEl = document.querySelector("#Timer");
 var highscoreEl = document.querySelector("#HighScore");
 var headerEl = document.querySelector("#header");
 var textEl = document.querySelector("#TextArea");
-var secondsLeft = 20;
+var secondsLeft = 200;
+var questionNumber = 0
+var answerKey = [3,2,4,3,4]
+var numberOfTimes = 0
 
 const questionsAnswers = [
     {Question: "Commonly used Data types DO NOT include:",
@@ -13,7 +16,7 @@ const questionsAnswers = [
     },
     {Question: "The condition in an if/else statement is enclosed within ____.",
     Answer1: "1. Quotes",
-    Answer2: "2. Curly Brakcets",
+    Answer2: "2. Curly Brackets",
     Answer3: "3. Parentheses",
     Answer4: "4. Square Brackets"
     },
@@ -38,17 +41,88 @@ const questionsAnswers = [
 
 
 ];
-                      
+           
+
  
 
 highscoreEl.textContent = "View High Scores"
 headerEl.textContent = "Test Test Test"
 //document.getElementById("HighScore").style.marginRight = "50px";
 
+
+
+
+
+
+
+
+
+function RunQuiz() {
+    console.log(questionNumber)
+    console.log(secondsLeft)
+  
+    headerEl.style.display = "none";
+    button5.style.display = "none";
+    button1.style.display = "block";
+    button2.style.display = "block";
+    button3.style.display = "block";
+    button4.style.display = "block";
+    textEl.textContent = questionsAnswers[questionNumber].Question
+    button1.textContent = questionsAnswers[questionNumber].Answer1
+    button2.textContent = questionsAnswers[questionNumber].Answer2
+    button3.textContent = questionsAnswers[questionNumber].Answer3
+    button4.textContent = questionsAnswers[questionNumber].Answer4
+    document.getElementById('button1').onclick = function(){answer('1');}
+    document.getElementById('button2').onclick = function(){answer('2');}
+    document.getElementById('button3').onclick = function(){answer('3');}
+    document.getElementById('button4').onclick = function(){answer('4');}
+    
+
+    function answer(x){
+
+      console.log("start answer function")
+      console.log(x)
+      var correctAnswer = (answerKey[numberOfTimes])
+      if(x == correctAnswer){
+        console.log("correct answer to question: " + numberOfTimes)
+        numberOfTimes++
+        questionNumber++
+        if(secondsLeft == 0 || secondsLeft < 0 || questionNumber == 6){
+          console.log("the end exam if statement worked!")
+        }
+      }
+      else{
+        console.log("incorrect answer to question: " + numberOfTimes)
+        numberOfTimes++
+        questionNumber++
+        secondsLeft = secondsLeft - 20
+        if(secondsLeft == 0 || secondsLeft < 0 || questionNumber == 6){
+          console.log("the end exam if statement worked!")
+        }
+      }
+     
+    
+      
+    }
+
+   
+
+   if (secondsLeft <= 0){
+     console.log("end quiz")
+
+     return;
+   }
+
+  
+
+}
+
 function QuizBody() {
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timeEl.textContent = "Time: " + secondsLeft ;
+
+      RunQuiz()
       
       if(secondsLeft === 0) {
         clearInterval(timerInterval);
